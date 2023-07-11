51e2dafcaac646155962a4eb75eaa3e57fc3b1a6
Nadi Duno <nadiduno@gmail.com>
2023-07-11 00:02:03 -0300

    Add CSS

--- main.js
+++ main.js
@@ -1,21 +1,22 @@
 function carregarPokemon() {
-    var urlApi = "https://pokeapi.co/api/v2/pokemon/25";
+    var idPokemon = document.getElementById("idPk").value
+    var urlApi = "https://pokeapi.co/api/v2/pokemon/" + idPokemon + "/";
 
     fetch(urlApi)
         .then((response) => {
             return response.json();
         })
         .then((data) => {
-            console.log(data)
+            // console.log(data)
             let imgPokemonFront = data["sprites"]["front_default"]
             let imgPokemonBack = data["sprites"]["back_default"]
+
             document.getElementById("nomPK").innerHTML = data["name"]
-            document.getElementById("idPk").innerHTML = data["id"]
             document.getElementById("imgPkFront").setAttribute('src', imgPokemonFront)
             document.getElementById("imgPkBack").setAttribute('src', imgPokemonBack)
 
         })
         .catch((error) => {
-            console.error("Erro na API")
+            console.error("Erro API Pokemon")
         })
 }
