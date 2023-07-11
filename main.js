538ead72061a20b5ea233ce275db91545cf975fe
Nadi Duno <nadiduno@gmail.com>
2023-07-10 22:42:04 -0300

    For Pokemon25

--- main.js
+++ main.js
@@ -1,19 +1,21 @@
-console.log('OK')
 function carregarPokemon() {
-    var valoprPk = document.getElementById('campoPk').value;
-    var urlApi = "https://pokeapi.co/api/v2/pokemon/" + valoprPk + "/";
-    console.log(valoprPk)
+    var urlApi = "https://pokeapi.co/api/v2/pokemon/25";
+
     fetch(urlApi)
         .then((response) => {
             return response.json();
         })
         .then((data) => {
-            // console.log(data)
-            document.getElementById("nomPK").innerHTML = data['name'];
-            document.getElementById("idPk").innerHTML = data['id'];
+            console.log(data)
+            let imgPokemonFront = data["sprites"]["front_default"]
+            let imgPokemonBack = data["sprites"]["back_default"]
+            document.getElementById("nomPK").innerHTML = data["name"]
+            document.getElementById("idPk").innerHTML = data["id"]
+            document.getElementById("imgPk").setAttribute('src', imgPokemonFront)
+            document.getElementById("imgPkFemale").setAttribute('src', imgPokemonBack)
 
         })
         .catch((error) => {
-            console(error)
+            console.error("Erro na API")
         })
 }
